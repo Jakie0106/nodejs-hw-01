8ee7c34e7df01ac6eea8af9b1bdd69d047bdfb7f
Jakie0106 <kamikadze777939@gmail.com>
2024-11-10 14:22:55 +0200

    task-8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.length > 0) {
+      contacts.pop();
+
+      await writeContacts(contacts);
+    } else {
+      console.log('No contacts');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
